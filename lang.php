c39bdedec4f5437c66cf3de24df576592d03d4f1
ajdonnison <ajdonnison>
2005-03-17 05:05:18 +0000

    New lang.php file - may be incorrect.

---
+++ lang.php
@@ -0,0 +1,12 @@
+<?php 
+// Language list for supported locales.
+// This is used in the 2.0 version of dotproject to correctly identify Windows language
+// support.  See the English version for details of its format.
+// Please Note - the original author did not create this lang.php file,
+// ajdonnison did, based upon whatever information could be gleaned from
+// The net.  If this is wrong blame me, not the original author, better
+// still just fix it and send me the details.
+$dir = basename(dirname(__FILE__));
+
+$LANGUAGES['no_NO'] = array( $dir, 'Norwegian', 'Norsk', 'norwegian');
+?>
